e3c4c427bba35cf43b6ce82563aa017f1dd9ce33
Brooke Dai <brookeydai@gmail.com>
2023-03-03 17:10:30 -0800

    initial type checker for STLC with Bool

---
+++ main.rkt
@@ -0,0 +1,99 @@
+#lang racket
+
+(require racket)
+(require rackunit
+         rackunit/text-ui)
+
+#|
+t ::=                   terms:
+  x                 variable
+  (lambda x T t)    abstraction
+  (t t)             application
+v ::=                   values:
+  True
+  False
+  (lambda x T t)    abstraction value
+T ::=                   types:
+  Bool              boolean
+  T -> T            type of functions
+G ::=                   contexts:
+  ()                empty context
+  ()            term variable binding
+|#
+
+;; Program examples
+(define val-true 'true)
+(define val-false 'false)
+(define val-identity '(lambda x Bool x))
+(define app-identity `(,val-identity ,val-false))
+
+;; Context examples
+(define empty-gamma #hash())
+(define gamma-1 '((x . Bool)))
+
+;; Type checker
+;; sexpr -> sexpr or error
+;; given a term t, return the type of t if t is well-typed
+;; otherwise, throw an error
+(define (type-check t)
+  
+  (define (is-primitive? s)
+    (match s
+      ['true #t]
+      ['false #t]
+      [_ #f]))
+
+  (define (primitive-type s)
+    (match s
+      ['true 'Bool]
+      ['false 'Bool]
+      [_ (error 'primitive-type "invalid primitive ~a" s)]))
+
+  ;; sexpr dict -> sexpr
+  (define (type-check-v v G)
+    (match v
+      [v
+      #:when (is-primitive? v)
+      (primitive-type v)]
+
+      [x 
+      (if (dict-has-key? G x)
+            (dict-ref G x)
+            (error 'type-check "key not found: ~a" x))]))
+
+  ;; sexpr dict -> (sexpr dict)
+  (define (type-check-t t G)
+    (match t
+      [`(lambda ,x ,T ,body)
+       (let ([T-body (type-check-t body (dict-set G x T))])
+         `(,T -> ,T-body))]
+
+      [`(,t1 ,t2)
+      (let* ([T-t1 (type-check-t t1 G)]
+             [T-t2 (type-check-t t2 G)])
+        (match T-t1
+          [`(,T-x -> ,T-body)
+           (if (eq? T-x T-t2)
+             `(,T-t1 ,T-t2)
+             (error 'type-check-t "type mismatch: ~a =/= ~a" T-t1 T-t2))]
+          [_ (error 'type-check-t "invalid type for application t1: ~a" T-t1)]))]
+
+      [x (type-check-v x G)]
+    ))
+  
+  (type-check-t t '()))
+
+;; Interpreter
+(define (interpret t)
+  (error "not implemented yet!"))
+
+;; Tests
+(define file-tests
+  (test-suite "tests for STLC with Bool"
+    (check-equal? (type-check val-true) 'Bool "true")
+    (check-equal? (type-check val-false) 'Bool "false")
+    (check-equal? (type-check val-identity) '(Bool -> Bool) "identity")
+    (check-equal? (type-check app-identity) '((Bool -> Bool) Bool) "app identity")
+  ))
+
+(run-tests file-tests)
